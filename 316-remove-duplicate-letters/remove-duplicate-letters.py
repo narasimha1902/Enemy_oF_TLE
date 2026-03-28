266504f2ba776d5bfa27596c1535503347e73b53
LAXMI NARASIMHA REDDY RAMIREDDY <narasimha19022007@gmail.com>
2026-03-29 04:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (70.20%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -4,10 +4,9 @@ class Solution:
         used=set()
         stack=[]
         for i,c in enumerate(s):
-            if c in used:
-                continue
-            while stack and c<stack[-1] and lu[stack[-1]]>i:
-                used.remove(stack.pop())
-            stack.append(c)
-            used.add(c)
+            if c not in used:
+                while stack and c<stack[-1] and lu[stack[-1]]>i:
+                    used.remove(stack.pop())
+                stack.append(c)
+                used.add(c)
         return "".join(stack) 
