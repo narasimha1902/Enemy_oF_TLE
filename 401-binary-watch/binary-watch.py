e29197a8a9c309fa8a56c3e6fea2a2a33fa02a07
LAXMI NARASIMHA REDDY RAMIREDDY <narasimha19022007@gmail.com>
2026-03-31 22:58:49 +0530

    Time: 3 ms (22.56%) | Memory: 19.2 MB (97.07%) - LeetSync

--- 401-binary-watch/binary-watch.py
+++ 401-binary-watch/binary-watch.py
@@ -1,19 +1,3 @@
 class Solution:
     def readBinaryWatch(self, t: int) -> List[str]:
-        if t>8:
-            return []
-        res=[]
-        mp=[8,4,2,1,32,16,8,4,2,1]
-        def bc(i,h,m,c):
-            if c==t and h<=11 and m<=59:
-                res.append(f"{h}:{m:02d}")
-                return
-            if c>t or h>11 or m>59 or i>9:
-                return
-            if i<4:
-                bc(i+1,h+mp[i],m,c+1)
-            else:
-                bc(i+1,h,m+mp[i],c+1)
-            bc(i+1,h,m,c)
-        bc(0,0,0,0)
-        return res
+        return [f"{h}:{m:02d}" for m in range(60) for h in range(12) if bin(h).count('1')+bin(m).count('1')==t]
