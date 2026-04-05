b9db84d7ab39f449c847259d56aee7babc56813b
LAXMI NARASIMHA REDDY RAMIREDDY <narasimha19022007@gmail.com>
2026-04-05 10:35:12 +0530

    Time: 284 ms (77.32%) | Memory: 24.1 MB (32.69%) - LeetSync

--- 4273-mirror-frequency-distance/mirror-frequency-distance.py
+++ 4273-mirror-frequency-distance/mirror-frequency-distance.py
@@ -1,14 +1,14 @@
 from collections import Counter
 class Solution:
     def mirrorFrequency(self, s: str) -> int:
-        ans=0
-        f=Counter(s)
-        for i in "abcdefghijklm":
-            m=chr(ord('a')+ord('z')-ord(i))
-            ans+=abs(f.get(i,0)-f.get(m,0))
-        for i in "01234":
-            m=chr(ord('0')+ord('9')-ord(i))
-            ans+=abs(f.get(i,0)-f.get(m,0))
+        f = Counter(s)
+        ans = 0
+        for i in range(13):
+            a = chr(ord('a') + i)
+            b = chr(ord('z') - i)
+            ans += abs(f[a] - f[b])
+        for i in range(5):
+            a = chr(ord('0') + i)
+            b = chr(ord('9') - i)
+            ans += abs(f[a] - f[b])
         return ans
-                
-            
