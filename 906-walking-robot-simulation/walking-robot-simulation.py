d29bff481e2396c9dece6a5adf292ca895d7e975
LAXMI NARASIMHA REDDY RAMIREDDY <narasimha19022007@gmail.com>
2026-04-06 12:15:29 +0530

    Time: 41 ms (42.36%) | Memory: 23.6 MB (93.49%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        obs=set(map(tuple,obstacles))
+        x,y=0,0
+        dirs=[(0,1),(1,0),(0,-1),(-1,0)]
+        dir=0
+        ans=0
+        for i in commands:
+            if i==-1:
+                dir=(dir+1)%4
+            elif i==-2:
+                dir=(dir-1)%4
+            else:
+                dx,dy=dirs[dir]
+                for _ in range(i):
+                    if (x+dx,y+dy) in obs:
+                        break
+                    else:
+                        x+=dx
+                        y+=dy
+                        ans=max(ans,x*x+y*y)
+        return ans
