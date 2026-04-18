473d4bb54e1b38669d0372c0e6931edf951ea397
LAXMI NARASIMHA REDDY RAMIREDDY <narasimha19022007@gmail.com>
2026-04-19 01:19:04 +0530

    Time: 92 ms (60.53%) | Memory: 59.6 MB (100.00%) - LeetSync

--- 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.py
+++ 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.py
@@ -1,17 +1,5 @@
 class Solution:
     def digArtifacts(self, n: int, artifacts: List[List[int]], dig: List[List[int]]) -> int:
-        f={}
-        ans=0
         dug=set(r*n+c for r,c in dig)
-        for t in artifacts:
-            a,b,c,d=t
-            claim=True
-            for i in range(a,c+1):
-                for j in range(b,d+1):
-                    if i*n+j not in dug:
-                        claim=False
-                        break
-            if claim:
-                ans+=1
-        return ans
+        return sum(all(i*n+j in dug for i in range(a,c+1) for j in range(b,d+1)) for a,b,c,d in artifacts)
             
