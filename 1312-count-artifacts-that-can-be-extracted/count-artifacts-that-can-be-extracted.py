29745a5840e70bb8c406db0fe7f83cffd5d7fbeb
LAXMI NARASIMHA REDDY RAMIREDDY <narasimha19022007@gmail.com>
2026-04-19 01:16:32 +0530

    Time: 69 ms (81.58%) | Memory: 59.9 MB (100.00%) - LeetSync

--- 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.py
+++ 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.py
@@ -1,22 +1,17 @@
 class Solution:
     def digArtifacts(self, n: int, artifacts: List[List[int]], dig: List[List[int]]) -> int:
         f={}
+        ans=0
+        dug=set(r*n+c for r,c in dig)
         for t in artifacts:
             a,b,c,d=t
-            idx=[]
+            claim=True
             for i in range(a,c+1):
                 for j in range(b,d+1):
-                    pos=i*n+j
-                    idx.append(pos)
-            for pos in idx:
-                f[pos]=idx
-        ans=0
-        digged={}
-        for r,c in dig:
-            pos=r*n+c
-            digged[pos]=True
-            if pos in f:
-                if all(i in digged for i in f[pos]):
-                    ans+=1
+                    if i*n+j not in dug:
+                        claim=False
+                        break
+            if claim:
+                ans+=1
         return ans
             
